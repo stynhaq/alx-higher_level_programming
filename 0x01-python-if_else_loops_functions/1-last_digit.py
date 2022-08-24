60f1cae7aa63a1e7c6e2fa5285735d7b830ae981
stynhaq <stynhaq@live.com>
2022-08-23 23:54:54 -0700

    fix pycodestyle for 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,10 +7,14 @@ negative_remainder = (abs(number) % 10) * -1
 
 if number >= 0:
     if positive_remainder > 5:
-        print(f"Last digit of {number} is {positive_remainder} and is greater than 5")
+        print(f"Last digit of {number} is {positive_remainder} \
+                and is greater than 5")
     elif positive_remainder == 0:
-        print(f"Last digit of {number} is {positive_remainder} and is 0")
+        print(f"Last digit of {number} is {positive_remainder}\
+                and is 0")
     else:
-        print(f"Last digit of {number} is {positive_remainder} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {positive_remainder} \
+                and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {negative_remainder} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {negative_remainder} \
+            and is less than 6 and not 0")
