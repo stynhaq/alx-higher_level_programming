7fb038bff97fca7108ca47ca16d02ec90f422d22
stynhaq <stynhaq@live.com>
2022-08-24 17:25:25 -0700

    fix 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for number in range(0, 9):
     for inner in range(number + 1, 10):
         if (number != 8) or (inner != 9):
-            print("{}{},".format(number, inner), end="")
+            print("{}{}, ".format(number, inner), end="")
         else:
             print("{}{}".format(number, inner))
