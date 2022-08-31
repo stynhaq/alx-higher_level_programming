1c0f146c5e35ac844e932da44ccb599b475cb9e1
stynhaq <stynhaq@live.com>
2022-08-30 19:12:28 -0700

    Batch

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,8 @@
-#!/bin/bash
-
+#!/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
